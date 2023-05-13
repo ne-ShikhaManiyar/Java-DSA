31c8b63babde934f69e4a736e8475c24b5a2a5d9
Mohak Talreja <mohaktalreja0011@gmail.com>
2023-05-13 18:38:38 +0530

    #Binary search algorithm in java

---
+++ Searching/Algorithms/binarySearch.java
@@ -0,0 +1,53 @@
+/* 
+   Binary search must be applied when
+   i)The data structure must be sorted.
+  ii) Access to any element of the data structure takes constant time.
+ * 
+ *   https://www.geeksforgeeks.org/binary-search/
+ */
+
+
+package Algorithms;
+import java.lang.*;
+import java.util.*;
+
+public class binarySearch{
+
+       public static void main(String[] args)
+    {
+         int A[] = {4,5,6,7,9,10};
+         int x = 9;
+         String str = Arrays.toString(A);
+        int res = Solve(A, x);
+        System.out.print(res);
+        
+    }
+
+    public static int Solve(int A[], int x)
+    {
+        int n = A.length;
+        int low = 0;
+        int high = n-1;
+     
+      while(low<=high)
+      {
+        int mid = low+(high-low)/2;        //calculating mid like this to avoid the space complexity issue
+        if(A[mid]==x)            // Check if x is present at mid
+        {
+            return mid;
+        }
+
+        if(A[mid]<x)           // If x greater than mid, discard left half
+                                // and move to right
+        {
+            low=mid+1;
+        }
+
+        else{                  // If x is smaller than mid, discard right half
+            high =mid-1;         // and move to left
+        }
+    }
+       return -1;              // if we reach here, then element was
+                                // not present
+    }
+}
