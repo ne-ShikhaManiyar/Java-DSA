9f94f594dc1a7286b632538577b3725bcdde2f89
Mohak Talreja <mohaktalreja0011@gmail.com>
2023-06-11 09:38:31 +0530

    #Togglecase in jave(upper to lower or vice versa)

--- Strings/Togglecase.java
+++ Strings/Togglecase.java
@@ -1,3 +1,37 @@
+/*
+ Problem Description
+You are given a character string A having length N, consisting of
+ only lowercase and uppercase latin letters.
+
+You have to toggle case of each character of string A. 
+For e.g 'A' is changed to 'a', 'e' is changed to 'E', etc.
+
+
+
+Problem Constraints
+1 <= N <= 105
+
+A[i] ∈ ['a'-'z', 'A'-'Z']
+
+Input 1:
+
+ A = "Hello" 
+Input 2:
+
+ A = "tHiSiSaStRiNg" 
+
+
+Example Output
+Output 1:
+
+ hELLO 
+Output 2:
+
+ ThIsIsAsTrInG 
+
+
+ */
+
 public class Togglecase {
 
     public static void main(String [] args)
