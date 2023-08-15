ea43563902db03ada488841f471ecf84949e5043
Mohak Talreja <mohaktalreja0011@gmail.com>
2023-08-15 19:40:14 +0530

    Updated Palindrome Code

--- LinkedList/PalindromeLL.java
+++ LinkedList/PalindromeLL.java
@@ -3,6 +3,23 @@
 Question : Palindrome LL
 https://leetcode.com/problems/palindrome-linked-list/
 
+Given the head of a singly linked list, return true if it is a 
+palindrome
+ or false otherwise.
+
+ Input: head = [1,2,2,1]
+Output: true
+
+Input: head = [1,2]
+Output: false
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+ 
+
+Follow up: Could you do it in O(n) time and O(1) space?
 
 
  */
