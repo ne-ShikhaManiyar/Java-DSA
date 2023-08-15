a8c111d2a72cd412e4b9a6b08e70f26062dd0dcb
Mohak Talreja <mohaktalreja0011@gmail.com>
2023-08-15 10:36:36 +0530

    Adding Question details for Reverse LL

--- LinkedList/ReverseLL.java
+++ LinkedList/ReverseLL.java
@@ -1,3 +1,26 @@
+/*
+ 
+Question : Reverse a LL
+https://leetcode.com/problems/reverse-linked-list/
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+
+Input: head = [1,2]
+Output: [2,1]
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+ 
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
+
 public class ReverseLL {
     
     public static class Node
