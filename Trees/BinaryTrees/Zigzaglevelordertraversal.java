7155cb26fcc62dfd7017e1d827fa71313af34e03
Mohak Talreja <mohaktalreja0011@gmail.com>
2023-09-25 16:34:46 +0530

    level order & zigzag level order

--- Trees/BinaryTrees/Zigzaglevelordertraversal.java
+++ Trees/BinaryTrees/Zigzaglevelordertraversal.java
@@ -1,11 +1,8 @@
-/*
-    https://leetcode.com/problems/binary-tree-level-order-traversal/ (leetcode 102)
- */
-
-
 import java.util.*;
-public class Levelordertraversalofbinarytree {
-    
+
+public class Zigzaglevelordertraversal
+{
+
 
     public class TreeNode {
              int val;
@@ -37,7 +34,8 @@ public class Levelordertraversalofbinarytree {
             return 1 + Math.max(height(root.left), height(root.right));
         }
         
-        public void nthlevel(TreeNode root, ArrayList<Integer> ans, int n) {
+         // left to right (level order )
+        public static void nthlevel(TreeNode root, ArrayList<Integer> ans, int n) {
             // Function to collect values of nodes at a specific level 'n'.
             if (root == null)
                 return;
@@ -53,6 +51,23 @@ public class Levelordertraversalofbinarytree {
             nthlevel(root.right, ans, n - 1);
         }
         
+        // right to left  (level order )
+        public static void nthlevel2(TreeNode root, ArrayList<Integer> ans, int n) {
+            // Function to collect values of nodes at a specific level 'n'.
+            if (root == null)
+                return;
+            if (n == 1) {
+                // If 'n' is 1, we've reached the desired level, so add the
+                // value of the current node to the 'ans' ArrayList.
+                ans.add(root.val);
+                return;
+            }
+        
+            // Recursively call 'nthlevel' on right and left children with 'n' decremented by 1.
+            nthlevel(root.right, ans, n - 1);
+            nthlevel(root.left, ans, n - 1);
+        }
+        
         public ArrayList<ArrayList<Integer>> solve(TreeNode root) {
             // Main function to perform level-order traversal and collect values at each level.
             int level = height(root) + 1; // Calculate the height of the tree.
@@ -64,7 +79,10 @@ public class Levelordertraversalofbinarytree {
             for (int i = 1; i <= level; i++) {
                 // Iterate through each level from 1 to the height.
                 ArrayList<Integer> ans = new ArrayList<>(); // Create a list for the current level.
-                nthlevel(root, ans, i); // Call 'nthlevel' to collect values at the current level.
+                if (i % 2 != 0)
+                    nthlevel(root, ans, i); // If the level is odd, use nthlevel.
+                else
+                    nthlevel2(root, ans, i); // If the level is even, use nthlevel2.
                 list.add(ans); // Add the current level's list to the result list.
             }
         
@@ -72,30 +90,19 @@ public class Levelordertraversalofbinarytree {
         }
 
 }
-
 /*
+  Explanation of above code
 
-Explanation of code
+  1)height Function:
+
+->This function calculates the height of a binary tree.
+->It starts from the root of the tree and recursively checks the height of the left and right subtrees.
+->The height of a node is defined as the length of the longest path from that node to a leaf node.
+-> The function returns the height of the tree.
 
 
-1) height of tree 
-->  This function calculates the height of the binary tree.
-->  It's a recursive function that returns the height of the tree rooted at the given node root. 
--> If the node is null or a leaf node (i.e., it has no left or right children), the height is
- considered to be 0. 
- -> Otherwise, it computes the height as 1 plus the maximum of the heights
-  of the left and right subtrees.
-
-nthlevel Function:
-
-2)This function is used to collect values of nodes at a specific level 'n' in a binary tree.
--> If the current node is null, it means you've reached the end of a branch, so the function returns.
--> If 'n' is 1, it means you've reached the desired level, and the function adds the
- value of the current node to the 'ans' ArrayList.
--> If 'n' is greater than 1, it means you need to traverse down one level. 
--> So, it recursively calls itself on the left and right children with 'n' decremented by 1.
--> when we call function intially asking the function to collect values at level n in the binary tree.
-
+2)nthlevel and nthlevel2 Functions:
+->These functions collect values of nodes at a specific level 'n' in the tree.
 Inside the function, n is checked(while doing n-1)
 -> If n is 1, it means you've reached the desired level, and you add the value of the current node
  to the ans ArrayList.
@@ -103,27 +110,27 @@ Inside the function, n is checked(while doing n-1)
  In this case, you need to traverse down to the next level. To do that, you recursively call nthlevel 
  on the left and right subtrees with n - 1. 
  -> This effectively reduces the level you're looking for by one and continues the traversal.
-
-
+->They start from the root and move down the tree, decrementing 'n' until 'n' becomes 1, 
+indicating that the desired level is reached.
+-> When 'n' becomes 1, they add the value of the current node to an ArrayList called 'ans.'
 
 3)solve Function:
-->This is the main function for performing a level-order traversal of the binary tree.
-->It calculates the height of the tree (in terms of levels) and stores it in the 'height' variable.
--> So, when you calculate the height with + 1, you are essentially
+
+-> This is the main function that performs a level-order traversal of the binary tree.
+-> It first calculates the height of the tree using the height function.
+> So, when you calculate the height with + 1, you are essentially
  counting the number of levels in the tree. If you don't add + 1,
   you are counting the number of edges along the longest path from the root to a leaf. 
--> It initializes an empty 'list' to store the result, which will be a list of lists
- containing values at each level.
--> If the 'root' is null, indicating an empty tree, it returns an empty 'list'.
--> It iterates from level 1 to the calculated height, and for each level:
--> It creates a new 'ans' ArrayList to store the values at that level.
--> Calls the 'nthlevel' function to collect values at the current level.
--> Adds the 'ans' list to the 'list' of lists.
--> Finally, it returns the 'list' containing values at each level.
+-> Then, it iterates through each level from 1 to the height.
+-> For each level, it creates an ArrayList called 'ans' to store the values of nodes at that level.
+-> If the level is odd, it calls the nthlevel function to collect values, 
+and if it's even, it calls the nthlevel2 function.
+-> The collected 'ans' lists for each level are added to a result list.
+-> Finally, the function returns the list of lists, where each inner list contains 
+values at a specific level of the tree.
 
-TC:o(n)
-SC : o(n)
+TC: o(n)
+SC: o(H)
 
 
-   
  */
